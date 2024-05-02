65e67957bb9d4e099308537b73be8f5f28317e09
arout2004 <ar6861273@gmail.com>
2024-05-02 11:24:33 +0530

    Link updated

--- client/src/apiCalls/auth.ts
+++ client/src/apiCalls/auth.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
-import { getCookie } from "cookies-next";
 
-const url = process.env.NEXT_PUBLIC_NODE_ENV === "development" ? "http://localhost:9000" : "";
+const url = process.env.NEXT_PUBLIC_NODE_ENV === "development" ? "http://localhost:9000" : "https://learn-mate-zeta.vercel.app";
 
 type sendOtpProps = {
     email: String;
